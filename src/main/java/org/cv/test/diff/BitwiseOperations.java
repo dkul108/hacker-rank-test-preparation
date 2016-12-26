8367f6ad5461d784a5199f415cfa5841e2f38b14
dkulinich <dkul108@gmail.com>
2016-12-27 00:27:11 +0100

    some clarifications while training with hackerrank.com

--- src/main/java/org/cv/test/diff/BitwiseOperations.java
+++ src/main/java/org/cv/test/diff/BitwiseOperations.java
@@ -1,4 +1,70 @@
 package org.cv.test.diff;
 
 public class BitwiseOperations {
+    public static void main(String[] args) {
+
+        int n = 16;
+        printBinaryform(n);
+        System.out.println("\n n=" + n +
+                "\n Integer.toBinaryString(n)=" + Integer.toBinaryString(n) +
+                "\n simplest decimal to octal: Integer.toString(n,8)="  + Integer.toString(n,8) +
+                "\n simplest decimal to binary: Integer.toString(n,2)=" + Integer.toString(n,2) +
+                "\n simplest decimal to Hex: Integer.toString(n,16)=" + Integer.toString(n,16)
+        );
+
+        int a = 60;	/* 60 = 0011 1100 */
+        int b = 13;	/* 13 = 0000 1101 */
+        int c = 0;
+
+        c = a & b;        /* 12 = 0000 1100 */
+        System.out.println("a & b = " + c );
+
+        c = a | b;        /* 61 = 0011 1101 */
+        System.out.println("a | b = " + c );
+
+        c = a ^ b;        /* 49 = 0011 0001 */
+        System.out.println("a ^ b = " + c );
+
+        c = ~a;           /*-61 = 1100 0011 */
+        System.out.println("~a = " + c );
+
+        c = a << 2;       /* 240 = 1111 0000 */
+        System.out.println("a << 2 = " + c );
+
+        c = a >> 2;       /* 15 = 1111 */
+        System.out.println("a >> 2  = " + c );
+
+        c = a >>> 2;      /* 15 = 0000 1111 */
+        System.out.println("a >>> 2 = " + c );
+    }
+
+
+
+    private static void printBinaryform(int number) {
+        int remainder;
+
+        if (number <= 1) {
+            System.out.print(number);
+            return;
+        }
+
+        remainder = number %2;
+        printBinaryform(number >> 1);
+        System.out.print(remainder);
+    }
+
+    public int[] convertBinaryNoRecursion(int no) {
+        int i = 0, temp[] = new int[7];
+        int binary[];
+        while (no > 0) {
+            temp[i++] = no % 2;
+            no /= 2;
+        }
+        binary = new int[i];
+        int k = 0;
+        for (int j = i - 1; j >= 0; j--) {
+            binary[k++] = temp[j];
+        }
+        return binary;
+    }
 }
---
+++ src/main/java/org/cv/test/numbers/OperatotionsWithNumbers.java
@@ -0,0 +1,25 @@
+package org.cv.test.numbers;
+
+/**
+ *   Name   Width in Bits   Range
+    double  64              1 .7e–308 to 1.7e+308
+    float   32              3 .4e–038 to 3.4e+038
+ */
+public class OperatotionsWithNumbers {
+    public static void main(String[] args) {
+        System.out.println("(double)3/(double)2 = " + (double)3/(double)2 );
+        System.out.println("3/2 = " + 3/2 );
+
+        System.out.println("2%3 = " + 2%3 );
+        System.out.println("(double)2%(double)3 = " + (double)2%(double)3 );
+
+        int n = 16;
+        System.out.println("\n n=" + n +
+                "\n Integer.toBinaryString(n)=" + Integer.toBinaryString(n) +
+                "\n simplest decimal to octal: Integer.toString(n,8)="  + Integer.toString(n,8) + " what?!" +
+                "\n simplest decimal to binary: Integer.toString(n,2)=" + Integer.toString(n,2) +
+                "\n simplest decimal to Hex: Integer.toString(n,16)=" + Integer.toString(n,16) + " what?!"
+        );
+
+    }
+}
