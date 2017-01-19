36c6f4cab97578c049f405aa2709d8f39d10898b
dkul108 <dkul108@gmail.com>
2017-01-19 17:00:29 +0100

    array based stack

--- src/main/java/org/cv/test/datastructures/Stack.java
+++ src/main/java/org/cv/test/datastructures/Stack.java
@@ -1,4 +1,32 @@
 package org.cv.test.datastructures;
 
-public class Stack {
+import java.util.ArrayList;
+
+public class Stack<T> {
+    private int maxSize;
+    private T[] stackArray;
+    private int top;
+
+    public Stack(int s) {
+        stackArray = (T [])new ArrayList<T>(maxSize).toArray();
+        maxSize = s;
+        top = -1;
+    }
+
+    public void push(T j) {
+        stackArray[++top] = j;
+    }
+
+    public T pop() {
+        return stackArray[top--];
+    }
+    public T peek() {
+        return stackArray[top];
+    }
+    public boolean isEmpty() {
+        return (top == -1);
+    }
+    public boolean isFull() {
+        return (top == maxSize - 1);
+    }
 }
